f4a1cddc33b4eabd3dc4625b008171465e6b1ff8
Narasimha devalla <dvkn002@gmail.com>
2022-09-18 15:13:15 +0530

    Updated files are added

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello Narasimhulu Devalla welcome to bangalore");
+        System.out.println("Hello  welcome to bangalore");
         System.out.println("Another commit");
         System.out.println("Added new statement in code ..... ");
         System.out.println("Added new statement in code by using pull command ..... ");
