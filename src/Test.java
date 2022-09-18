7ecdb61bf23413d43f0053bfb1235db6ce1eca2f
unknown <narasimha.devalla@gmail.com>
2022-09-18 12:26:47 +0530

    latest changes in Test.java

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,7 @@ public class Test {
         System.out.println("Hello Narasimhulu Devalla welcome to bangalore");
         System.out.println("Another commit");
         System.out.println("Added new statement in code ..... ");
+        System.out.println("Added new statement in code by using pull command ..... ");
     }
 
 }
